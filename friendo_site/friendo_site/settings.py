09b1d98333257a7277230c7099a1c5eb6b465aea
fisher60 <wakefollower@gmail.com>
2020-09-07 20:16:10 -0700

    Feature: Custom user model
    The custom user model needs to be created early to prevent future issues. This will allow custom user model to be expanded in the future.

--- friendo_site/friendo_site/settings.py
+++ friendo_site/friendo_site/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yin)59ez#ba_z=9%&k704(ur@=xt!c7*2r^+j+13)*=-f3rb4x'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_SECRET_KEY", None)
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom user model
+AUTH_USER_MODEL = "users.User"
