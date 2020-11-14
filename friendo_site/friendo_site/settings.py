f608b713d4b2f1a6bd7b6cc432dde8029df09a1a
fisher60 <wakefollower@gmail.com>
2020-11-14 11:10:07 -0800

    revert last

--- friendo_site/friendo_site/settings.py
+++ friendo_site/friendo_site/settings.py
@@ -129,8 +129,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = ["/static/"]
-
 STATIC_URL = "/static/"
 STATIC_ROOT = "/static/"
 
