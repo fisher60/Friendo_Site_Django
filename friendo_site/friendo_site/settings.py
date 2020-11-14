0113fb9d487999e19c7e6357b8be428ed4b2e440
fisher60 <wakefollower@gmail.com>
2020-11-13 19:06:45 -0800

    add static root

--- friendo_site/friendo_site/settings.py
+++ friendo_site/friendo_site/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = "/static"
 
 # Custom user model
 AUTH_USER_MODEL = "users.User"
