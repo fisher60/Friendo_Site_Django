6b64691c883a58c3caef72f3118b832300491044
fisher60 <wakefollower@gmail.com>
2020-11-13 18:34:47 -0800

    bugfix/load debug as bool

--- friendo_site/friendo_site/settings.py
+++ friendo_site/friendo_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = bool(os.environ.get("DEBUG", False))
 
 ALLOWED_HOSTS = []
 
