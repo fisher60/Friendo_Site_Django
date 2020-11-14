d70403011d1ce606518935f24bf12dbe3fee5a7f
fisher60 <wakefollower@gmail.com>
2020-11-13 18:54:19 -0800

    fix docker compose to read all env vars from .env

--- friendo_site/friendo_site/settings.py
+++ friendo_site/friendo_site/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", None)
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", False))
 
 ALLOWED_HOSTS = []
