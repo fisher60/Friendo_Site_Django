397ec39ff50d7e63d9437a944122732a8cb832f8
fisher60 <wakefollower@gmail.com>
2020-11-14 00:10:05 -0800

    change static url to absolute url with http

--- friendo_site/friendo_site/settings.py
+++ friendo_site/friendo_site/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "http://www.friendo.us/static/"
 STATIC_ROOT = "/static"
 
 # Custom user model
