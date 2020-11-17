1d4b5f87a4cd1468646faadfb81c1d8f487a622b
fisher60 <wakefollower@gmail.com>
2020-11-16 18:20:08 -0800

    remove testing static url

--- friendo_site/friendo_site/urls.py
+++ friendo_site/friendo_site/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("friendo_site.graphql.urls")),
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
