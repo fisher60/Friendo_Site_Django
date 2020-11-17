531ba76e69ecef5a3a39ef6cf4888d5db9077648
fisher60 <wakefollower@gmail.com>
2020-11-16 17:38:28 -0800

    change from external volume to cp script

--- friendo_site/friendo_site/settings.py
+++ friendo_site/friendo_site/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/static"
+STATIC_ROOT = "/code/static"
 
 # Custom user model
 AUTH_USER_MODEL = "users.User"
