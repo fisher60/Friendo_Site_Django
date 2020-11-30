7b37322d0c288272235daa676ca80c316ba88e7a
fisher60 <wakefollower@gmail.com>
2020-11-29 22:58:31 -0800

    add login and user registration
    This commit adds the start of a user login and registration system, this is NOT production ready and would make the api VERY vulnerable. Permissions need to be organized to allow staff and the bot access to api, but not average users. This also adds view for discord auth, account verification needs to be added to connect discord data to user profile.

--- friendo_site/friendo_site/users/urls.py
+++ friendo_site/friendo_site/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import discord_login, discord_login_redirect
+from .views import discord_login, discord_login_redirect, login_view, register
 
 urlpatterns = [
-    path("login/", discord_login, name="login"),
+    path("discord-auth/", discord_login, name="discord-auth"),
+    path("login/", login_view, name="login"),
+    path("register/", register, name="register"),
     path("auth_redirect", discord_login_redirect, name="auth_redirect"),
 ]
