93d8f4602ec8737b33de8259ea9b9f565b68bd2e
fisher60 <wakefollower@gmail.com>
2021-04-07 15:07:55 -0700

    add dogeboard_reactions_required

--- friendo_site/friendo_site/servers/models.py
+++ friendo_site/friendo_site/servers/models.py
@@ -5,6 +5,7 @@ class Server(models.Model):
     server_id = models.CharField(max_length=60, unique=True)
     dogeboard_id = models.CharField(max_length=60, blank=True, null=True)
     dogeboard_emoji = models.CharField(max_length=60, blank=True, null=True)
+    dogeboard_reactions_required = models.IntegerField(default=5)
 
     def __str__(self):
         return f"Server Id: {self.server_id}"
