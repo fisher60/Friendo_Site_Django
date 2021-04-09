dcaeb66589bcaa978d0e21d4274b7a7d8e489dcb
fisher60 <wakefollower@gmail.com>
2021-04-09 16:48:07 -0700

    adds a working version of the discord server/guild model and the api functionality for it
    The code should also be refactored to reference 'guild' instead of 'server' in all locations.

--- friendo_site/friendo_site/graphql/types/mutation.py
+++ friendo_site/friendo_site/graphql/types/mutation.py
@@ -1,8 +1,10 @@
 from ariadne import MutationType
 from .users import resolve_login, get_user
-from .servers import get_server
+from .servers import get_server, create_server, update_server
 
 mutation = MutationType()
 mutation.set_field("login", resolve_login)
 mutation.set_field("user", get_user)
-mutation.set_field("server", get_server)
+mutation.set_field("get_guild", get_server)
+mutation.set_field("create_guild", create_server)
+mutation.set_field("update_guild", update_server)
--- friendo_site/friendo_site/servers/models.py
+++ friendo_site/friendo_site/servers/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Server(models.Model):
-    server_id = models.CharField(max_length=60, unique=True)
-    dogeboard_id = models.CharField(max_length=60, blank=True, null=True)
+    guild_id = models.BigIntegerField(unique=True)
+    dogeboard_id = models.BigIntegerField(blank=True, null=True)
     dogeboard_emoji = models.CharField(max_length=60, blank=True, null=True)
     dogeboard_reactions_required = models.IntegerField(default=5)
 
     def __str__(self):
-        return f"Server Id: {self.server_id}"
+        return f"Server Id: {self.guild_id}"
