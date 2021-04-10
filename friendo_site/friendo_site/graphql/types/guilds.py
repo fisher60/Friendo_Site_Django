4ac9db6c8b5d730fb038f18f9a5f71047bedd123
fisher60 <wakefollower@gmail.com>
2021-04-10 14:02:15 -0700

    add validation for dogeboard reactions required

--- friendo_site/friendo_site/graphql/types/guilds.py
+++ friendo_site/friendo_site/graphql/types/guilds.py
@@ -10,28 +10,27 @@ def get_guild(_, info, data):
         raise KeyError("guild_id is missing")
 
 
-@token_required
-def create_guild(_, info, data):
-    if data.get("guild_id", None):
-        new_guild = Guild(guild_id=int(data.get("guild_id")))
-        new_guild.save()
-        return new_guild
-    else:
-        raise KeyError("guild_id is missing")
+# @token_required
+# def create_guild(_, info, data):
+#     if data.get("guild_id", None):
+#         new_guild = Guild(guild_id=int(data.get("guild_id")))
+#         new_guild.save()
+#         return new_guild
+#     else:
+#         raise KeyError("guild_id is missing")
 
 
 @token_required
-def update_guild(_, info, data):
-    if data.get("guild_id", None):
-        this_guild = Guild.objects.get(guild_id=data.get("guild_id", None))
-        if data.get("dogeboard_id", None):
-            this_guild.dogeboard_id = int(data.get("dogeboard_id", None))
-        if data.get("dogeboard_emoji", None):
-            this_guild.dogeboard_emoji = data.get("dogeboard_emoji", None)
-        if data.get("dogeboard_reactions_required", None):
-            this_guild.dogeboard_reaction_required = int(
-                data.get("dogeboard_reactions_required", None)
-            )
+def modify_guild(_, info, data):
+    if guild_id := data.get("guild_id", None):
+        this_guild, _ = Guild.objects.get_or_create(guild_id=guild_id)
+
+        if doge_id := data.get("dogeboard_id", None):
+            this_guild.dogeboard_id = int(doge_id)
+        if doge_emoji := data.get("dogeboard_emoji", None):
+            this_guild.dogeboard_emoji = doge_emoji
+        if reactions_req := data.get("dogeboard_reactions_required", None):
+            this_guild.dogeboard_reactions_required = int(reactions_req)
 
         this_guild.save()
         return this_guild
--- friendo_site/friendo_site/graphql/types/mutation.py
+++ friendo_site/friendo_site/graphql/types/mutation.py
@@ -1,10 +1,9 @@
 from ariadne import MutationType
 from .users import resolve_login, get_user
-from .guilds import get_guild, create_guild, update_guild
+from .guilds import get_guild, modify_guild
 
 mutation = MutationType()
 mutation.set_field("login", resolve_login)
 mutation.set_field("user", get_user)
 mutation.set_field("get_guild", get_guild)
-mutation.set_field("create_guild", create_guild)
-mutation.set_field("update_guild", update_guild)
+mutation.set_field("modify_guild", modify_guild)
